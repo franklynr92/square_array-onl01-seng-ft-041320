8054ffa97d764c3547a2a2ae3f1d1152e982dd4a
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-16 13:51:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,12 @@
-  array=[1, 2, 3]
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
   
 def square_array(array)
 
-  array.each do |square|
+ arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
     
    end 
 end
